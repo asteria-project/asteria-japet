1c24f70c96a73a4fb8ba69d347539c6ddbc7862a
Pascal ECHEMANN <pascal@wooz.io>
2019-05-10 15:06:01 +0200

    gitignore update
    Fix util s folder import.

---
+++ utils/types/japet.d.ts
@@ -0,0 +1,12 @@
+/*!
+ * Asteria Japet Node Module
+ * Copyright(c) 2019 Pascal ECHEMANN
+ * Apache 2.0 Licensed
+ * This is a part of the Asteria Project: <https://github.com/asteria-project>
+ */
+
+declare module "asteria-japet" {
+
+import { FilterCondition, FilterDefinition, AbstractAsteriaObject } from "asteria-gaia";
+
+/*PLACEHOLDER*/}
