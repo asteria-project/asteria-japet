4d260f706fcee111d8243bee7d33e7c23d0cda59
Pascal ECHEMANN <pechemann@gmail.com>
2019-08-16 15:16:24 +0200

    Test update
    Add test suite for the AsqlParser class.

---
+++ test/com/asteria/japet/lang/AsqlParserTest.ts
@@ -0,0 +1,50 @@
+import 'mocha';
+import { expect, assert } from 'chai';
+
+// Class to test:
+import { AsqlParser } from '../../../../../src/com/asteria/japet/lang/AsqlParser';
+import { AsqlToken } from '../../../../../src/com/asteria/japet.index';
+
+// Utilities:
+import * as utils from '../../../../../utils/test-utils/utilities/AsqlParserTestUtils';
+import * as tokens from '../../../../../utils/test-utils/utilities/TokenTestUtils';
+import { AsteriaException } from 'asteria-gaia';
+
+// Test:
+describe('AsqlParser class test', ()=> {
+
+    describe('#getClassName()', ()=> {
+        it('should return the AsqlParser fully qualified class name', ()=> {
+            const parser: AsqlParser = new AsqlParser();
+            expect(parser.getClassName()).to.equal(utils.CLASS_NAME);
+        });
+    });
+    
+    describe('#parse()', ()=> {
+        it('should return the correct list of tokens', ()=> {
+            const parser: AsqlParser = new AsqlParser();
+            const result: Array<AsqlToken> = parser.parse(utils.VALID_STRING);
+            result.forEach((token: AsqlToken, index: number)=> {
+                const tokenRef: AsqlToken = tokens.TOKEN_LIST[index];
+                expect(token.type).to.equal(tokenRef.type);
+                expect(token.value).to.equal(tokenRef.value);
+            });
+        });
+
+        it('should throw an axception when string operand does not end with a quote character', ()=> {
+            const parser: AsqlParser = new AsqlParser();
+            const shouldThrowError: Function = () => {
+                parser.parse(utils.INVALID_TEXT_STRING);
+            };
+            assert.throws(shouldThrowError, AsteriaException, 'asql string operand must end with a quote character');
+        });
+        
+        it('should throw an axception when number operand ends with a quote character', ()=> {
+            const parser: AsqlParser = new AsqlParser();
+            const shouldThrowError: Function = () => {
+                parser.parse(utils.INVALID_NUMBER_STRING);
+            };
+            assert.throws(shouldThrowError, AsteriaException, 'asql number operand must not end with a quote character');
+        });
+    });
+});
---
+++ utils/test-utils/utilities/AsqlParserTestUtils.ts
@@ -0,0 +1,9 @@
+/*!
+ * This module constains utilities used by the AsqlParser test suite.
+ */
+
+// Utilities:
+export const CLASS_NAME: string = 'com.asteria.japet.lang::AsqlParser';
+export const VALID_STRING: string = 'Population > 1000000 AND Country = \'us\'';
+export const INVALID_NUMBER_STRING: string = 'Population > 1000000\' AND Country = \'us\'';
+export const INVALID_TEXT_STRING: string = 'Population > 1000000 AND Country = \'us';
